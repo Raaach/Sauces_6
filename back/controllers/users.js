b9bc18b586d90c75b72604e02823603e0a7e126a
Rachid Marir <marirrachid@yahoo.fr>
2023-08-26 15:03:15 +0200

    creatEmail token

--- back/controllers/users.js
+++ back/controllers/users.js
@@ -1,20 +1,23 @@
 const {User}= require("../mongo")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 
 async function createUser(req, res)  {
+    try{
     const email = req.body.email
     const password = req.body.password          // idem: const (email, password)= req.body
 
     const hashedPassword = await hashPassword(password)
 
     const user = new User({email, password: hashedPassword})
-
-    user
-        .save()
-        .then(()=> res.status(201).send({message : "Utilisateur enregistré !"})) //(201)= ressource créée
-        .catch((err)=> res.status(409).send({message: "User pas enregitré:" + err})) //(409)= conflit
+    await user.save()
+    res.status(201).send({message : "Utilisateur enregistré !"}) //(201)= ressource créée
     }
+    catch(err){
+            res.status(409).send({message: "User pas enregitré:" + err}) //(409)= conflit
+    }
+}
 
 function hashPassword(password) {
     const saltRounds = 10
@@ -22,6 +25,7 @@ function hashPassword(password) {
 }
 
 async function logUser(req,res){
+    try{
     const email = req.body.email
     const password = req.body.password
     const user = await User.findOne({ email: email})
@@ -30,11 +34,18 @@ async function logUser(req,res){
     if (!isPasswordOk) {
         res.status(403).send({message: 'Mot de passe incorecte'})
     }
-    res.status(200).send({message: 'Mot de passe correct'})
-    
-    console.log('user:', user)
-    console.log('isPasswordOk:', isPasswordOk)
-    
+    const token = creatEmailToken(email)
+    res.status(200).send({userId: user?._id, token: token})
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).send({message:" Erreur interne"})
+    }
 }
 
+function creatEmailToken(email){
+    const passwordJwt = process.env.JWT_PASSWORD
+    return jwt.sign({email: email}, passwordJwt, {expiresIn: "12h"})
+}
+//User.deleteMany({}).then(()=>console.log("all removed"))
 module.exports ={createUser, logUser}
