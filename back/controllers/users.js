70fdb5def0fefd333d01cc17152c01cdd7f185b9
Rachid Marir <marirrachid@yahoo.fr>
2023-08-26 12:38:44 +0200

    bcrypt/hashPassword

--- back/controllers/users.js
+++ back/controllers/users.js
@@ -1,15 +1,24 @@
 const {User}= require("../mongo")
+const bcrypt = require("bcrypt")
 
 
-function createUser(req, res)  {
+async function createUser(req, res)  {
     const email = req.body.email
     const password = req.body.password          // idem: const (email, password)= req.body
-    const user = new User({email, password})
+
+    const hashedPassword = await hashPassword(password)
+
+    const user = new User({email, password: hashedPassword})
 
     user
         .save()
         .then(()=> res.send({message : "Utilisateur enregistré !"}))
         .catch(err=> console.log("User pas enregitré!", err))
     }
+
+function hashPassword(password) {
+    const saltRounds = 10
+    return bcrypt.hash(password, saltRounds)
+}
 
 module.exports ={createUser}
